e0dc3ecd71249bfee253979bd5726dd76faf3c7e
Sergey Lomov <SSpirit10000@gmail.com>
2024-02-08 17:46:01 +0300

    WIP: Memory node implementation

--- game/emotion_type.gd
+++ game/emotion_type.gd
@@ -1,6 +1,7 @@
 class_name EmotionType
 
 enum {
+	NONE,	#Not really an amotion, but emotion type value that means null
 	JOY,
 	TRUST,
 	FEAR,
--- memory/nodes/core/node_core.gd
+++ memory/nodes/core/node_core.gd
@@ -1,3 +1,38 @@
-class_name MemoryNodeCore extends Node
+class_name MemoryNodeCore extends Sprite2D
 
-var 
+enum State {
+	NORMAL,
+	ECLIPSE,
+	OBLIVION,
+	OVERCHARGE
+}
+
+var shader = preload("res://memory/nodes/core/node_core.gdshader")
+
+var size: Vector2
+var rect: Rect2:
+	get: return Rect2(position - size * 0.5, size)
+
+var state: State
+var primary_emotion: EmotionType
+var secondary_emotion: EmotionType
+var energy: float
+var ether: float
+
+var transmutation_speed: float
+var transmutation_cost: float
+var energy_capacity: float
+var ether_capacity: float
+var awakening_energy: float
+
+func _ready():
+	material = ShaderMaterial.new()
+	material.shader = shader
+	update_shader_params()
+	
+func _draw():
+	draw_rect(rect, Color.WHITE)
+
+func update_shader_params():
+	#material.set_shader_parameter("")
+	pass
